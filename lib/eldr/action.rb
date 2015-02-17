172c7938e7cf1198f141d4e349598afb5a8a7514
K-2052 <k@2052.me>
2015-02-16 21:48:46 -0800

    drop configuration methods

--- lib/eldr/action.rb
+++ lib/eldr/action.rb
@@ -31,19 +31,6 @@ module Eldr
       env['eldr.params']
     end
 
-    def self.configuration
-      @configuration ||= Configuration.new()
-    end
-
-    def initialize(configuration: Configuration.new)
-      @configuration = configuration
-      @configuration.merge! self.class.configuration.to_h
-    end
-
-    def config
-      configuration
-    end
-
     def to_a
       [status, header, [body]]
     end
